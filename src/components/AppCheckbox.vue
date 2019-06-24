d425fe91de3495267e416f9ec31e6aed9f1bd95f
David Pavel <David.Pavel@cvut.cz>
2019-06-24 12:04:47 +0200

    Add example of slot documentation bug when <slot> is inside of <template>.

---
+++ src/components/AppCheckbox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <label>
+            <input type="checkbox"/>
+            <!-- @slot Label of checkbox -->
+            <slot #label/>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppCheckbox"
+    };
+</script>
---
+++ src/components/CheckboxGroup.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-checkbox v-for="option in options" :key="option">
+            <template #label>
+                <!-- @slot Content of checkbox label delegated to the AppCheckbox component -->
+                <slot #label>{{option}}</slot>
+            </template>
+        </app-checkbox>
+    </div>
+</template>
+
+<script>
+    import AppCheckbox from "./AppCheckbox";
+    export default {
+        name: "CheckboxGroup",
+        components: {AppCheckbox},
+        props: {
+            options: {
+                type: Array,
+                required: true
+            },
+        },
+    };
+</script>
